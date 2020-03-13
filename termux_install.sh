e5f6e720d7d4f09b8e24ee23f9deb213577b8237
Azrim <mirzaspc@gmail.com>
2020-03-13 22:38:16 +0700

    termux_install.sh: fix derp

--- termux_install.sh
+++ termux_install.sh
@@ -35,7 +35,7 @@ pkg update && pkg upgrade -y
 pkg install clang curl git libcrypt libffi libiconv libjpeg* libjpeg-turbo libwebp libxml2 libxslt make ndk-sysroot openssl postgresql python readline wget zlib -y
 
 git clone https://github.com/aone-id/aone-kangbot.git
-cd OpenUserBot
+cd aone-kangbot
 
 pip install --upgrade pip setuptools
 pip install -r requirements.txt
