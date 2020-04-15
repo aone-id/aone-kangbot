df0dbfc7129b23e38bebe65a662d7521a6e8a61e
yincen17 <52388255+yincen17@users.noreply.github.com>
2020-04-14 21:47:26 -0700

    make better

--- userbot/modules/qrcode.py
+++ userbot/modules/qrcode.py
@@ -93,17 +93,10 @@ async def make_qr(makeqr):
 
 
 CMD_HELP.update({
-    'qr':
+    'makeqr':
     ".makeqr <content>\
 \nUsage: Make a QR Code from the given content.\
 \nExample: .makeqr www.google.com\
 \nNote: use .decode <reply to barcode/qrcode> to get decoded content."
 })
 
-CMD_HELP.update({
-    'barcode':
-    ".barcode <content>\
-\nUsage: Make a BarCode from the given content.\
-\nExample: .barcode www.google.com\
-\nNote: use .decode <reply to barcode/qrcode> to get decoded content."
-})
