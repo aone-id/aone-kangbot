af591dadba77f2b4c54526d29d0bda804aa07d7d
Busetdah <52714390+Busetdah@users.noreply.github.com>
2020-04-15 10:39:00 +0700

    Update events.py

--- userbot/events.py
+++ userbot/events.py
@@ -134,11 +134,6 @@ def register(**args):
                     file.close()
 
                     if LOGSPAMMER:
-                        await check.client.respond(
-                            "`Sorry, my userbot has crashed.\
-                        \nThe error logs are stored in the userbot's log chat.`"
-                        )
-
                     await check.client.send_file(send_to,
                                                  "error.log",
                                                  caption=text)
