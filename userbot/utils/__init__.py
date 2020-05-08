659be6ec72f3131041584ad7cd3c7f5a6911a8db
Adek Maulana <adek@techdro.id>
2020-05-09 05:46:59 +0700

    ProjectBish: move chromedriver into it's own file in utils
    Change-Id: I86985b7f0f87a1aaf5d320204591560ebdb33667

--- userbot/utils/__init__.py
+++ userbot/utils/__init__.py
@@ -14,13 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with TG-UserBot.  If not, see <https://www.gnu.org/licenses/>.
 
-# Copyright (C) 2019 Adek Maulana
-#
-# Licensed under the Raphielscape Public License, Version 1.d (the "License");
-# you may not use this file except in compliance with the License.
-#
-
-from .chrome import chrome, options
+from .chrome import chrome
 from .progress import progress
 
 from .tools import (
