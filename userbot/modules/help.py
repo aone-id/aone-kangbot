6f2d7961a5464cba20b58736094148695421db62
Takeshiro04 <61047914+Takeshiro04@users.noreply.github.com>
2020-05-30 21:39:57 +0800

    test on a new .help module

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -1,9 +1,9 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
+# Copyright (C) 2020 The Raphielscape Company LLC.
 #
-# Licensed under the Raphielscape Public License, Version 1.c (the "License");
+# Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
 #
-""" Userbot help command """
+""" Paperplane's help command """
 
 from userbot import CMD_HELP
 from userbot.events import register
@@ -11,26 +11,25 @@ from userbot.events import register
 
 @register(outgoing=True, pattern="^.help(?: |$)(.*)")
 async def help(event):
-    """ For .help command,"""
+    """ For .help command"""
     args = event.pattern_match.group(1).lower()
+
     if args:
         if args in CMD_HELP:
-            await event.edit(str(CMD_HELP[args]))
+            await event.edit(
+                f"Here is some help for the **{CMD_HELP[args][0]}** module:\n\n"
+                + str(CMD_HELP[args][1])
+            )
         else:
-            await event.edit("Please specify a valid module name.")
+            await event.edit(
+                f"Help string for {args} not found! Type ```.help``` to see valid module names."
+            )
     else:
-        head = "**Help for** [Aone-Kangbot](https://github.com/aone-id/aone-kangbot)"
-        head2 = "For more information to use command,"
-        head3 = "send `.help <module name>`"
-        head4 = "List for all available command below: "
         string = ""
-        separator = "=============================================="
-        for i in CMD_HELP:
-            string += "`" + str(i)
-            string += "` | "
-        await event.edit(f"{head}\
-              \n{head2}\
-              \n{head3}\
-              \n{separator}\
-              \n{head4}\
-              \n\n{string}")
+        for i in CMD_HELP.values():
+            string += f"`{str(i[0])}`, "
+        string = string[:-2]
+        await event.edit(
+            "Please specify which module you want help for!\n\n"
+            f"{string}"
+        )
