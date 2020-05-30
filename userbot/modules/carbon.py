4ecd20588d899bbe718e2ffc480f32791b2a45e8
Azrim <mirzaspc@gmail.com>
2020-05-30 12:27:11 +0700

    modules: carbon: update *simplify help *explain command properly *remove carbon5 (not working)
    Signed-off-by: azrim <mirzaspc@gmail.com>

--- userbot/modules/carbon.py
+++ userbot/modules/carbon.py
@@ -314,103 +314,22 @@ async def carbon_api(e):
     driver.quit()
     # Removing carbon.png after uploading
     await e.delete()  # Deleting msg
-    
-
-@register(outgoing=True, pattern="^.carbon5")
-async def carbon_api(e):
-    """ A Wrapper for carbon.now.sh """
-    await e.edit("`Processing..`")
-    CARBON = 'https://carbon.now.sh/?bg=rgba(239%2C40%2C44%2C1)&t=one-light&wt=none&l=application%2Ftypescript&ds=true&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=56px&ph=56px&ln=false&fl=1&fm=Hack&fs=14px&lh=143%25&si=false&es=2x&wm=false&code={}'
-    global CARBONLANG
-    textx = await e.get_reply_message()
-    pcode = e.text
-    if pcode[8:]:
-        pcode = str(pcode[8:])
-    elif textx:
-        pcode = str(textx.message)  # Importing message to module
-    code = quote_plus(pcode)  # Converting to urlencoded
-    await e.edit("`Processing..\n25%`")
-    if os.path.isfile("/root/userbot/.bin/carbon.png"):
-        os.remove("/root/userbot/.bin/carbon.png")
-    url = CARBON.format(code=code, lang=CARBONLANG)
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.binary_location = GOOGLE_CHROME_BIN
-    chrome_options.add_argument("--window-size=1920x1080")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-gpu")
-    prefs = {'download.default_directory': '/root/userbot/.bin'}
-    chrome_options.add_experimental_option('prefs', prefs)
-    driver = webdriver.Chrome(executable_path=CHROME_DRIVER,
-                              options=chrome_options)
-    driver.get(url)
-    await e.edit("`Processing..\n50%`")
-    download_path = '/root/userbot/.bin'
-    driver.command_executor._commands["send_command"] = (
-        "POST", '/session/$sessionId/chromium/send_command')
-    params = {
-        'cmd': 'Page.setDownloadBehavior',
-        'params': {
-            'behavior': 'allow',
-            'downloadPath': download_path
-        }
-    }
-    command_result = driver.execute("send_command", params)
-    driver.find_element_by_xpath("//button[contains(text(),'Export')]").click()
-   # driver.find_element_by_xpath("//button[contains(text(),'4x')]").click()
-   # driver.find_element_by_xpath("//button[contains(text(),'PNG')]").click()
-    await e.edit("`Processing..\n75%`")
-    # Waiting for downloading
-    while not os.path.isfile("/root/userbot/.bin/carbon.png"):
-        await sleep(0.5)
-    await e.edit("`Processing..\n100%`")
-    file = '/root/userbot/.bin/carbon.png'
-    await e.edit("`Uploading..`")
-    await e.client.send_file(
-        e.chat_id,
-        file,
-        caption="Made using [Carbon](https://carbon.now.sh/about/),\
-        \na project by [Dawn Labs](https://dawnlabs.io/)",
-        force_document=True,
-        reply_to=e.message.reply_to_msg_id,
-    )
-
-    os.remove('/root/userbot/.bin/carbon.png')
-    driver.quit()
-    # Removing carbon.png after uploading
-    await e.delete()  # Deleting msg
-    
 
 
- 
-    
+
 CMD_HELP.update({
-    'carbon1':
-    '.carbon1 <text> [or reply]\
-        \nUsage: Beautify your text'
-})
-
-    
-CMD_HELP.update({
-    'carbon2':
-    '.carbon2 <text> [or reply]\
-        \nUsage: Beautify your TXT.'
-})
-
-    
-CMD_HELP.update({
-    'carbon3':
-    '.carbon3 <text> [or reply]\
-        \nUsage: Beautify your text.'
-})
-CMD_HELP.update({
-    'carbon4':
-    '.carbon4 <text> [or reply]\
-        \nUsage: Beautify your text.'
-})
-CMD_HELP.update({
-    'carbon5':
-    '.carbon5 <text> [or reply]\
-        \nUsage: Beautify your text.'
+    "carbon":
+    "`.carbon1` <text> [or reply]\
+\nUsage: Beautify your text\
+\n(Purple text)\
+\n\n`.carbon2` <text> [or reply]\
+\nUsage: Beautify your text.\
+\n(Light Panel, Red Background)\
+\n\n`.carbon3` <text> [or reply]\
+\nUsage: Beautify your text.\
+\n(Dark Panel, Blue Background)\
+\n\n`.carbon4` <text> [or reply]\
+\nUsage: Beautify your text.\
+\n(Light Panel, Blue Background)\
+"
 })
