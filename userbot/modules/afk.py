af94a047609da17b9cf7d6a94eeaa030bcbae21a
azrim <mirzaspc@gmail.com>
2020-06-04 17:15:24 +0700

    modules: afk: rework * Back to our coding style * use replase instead slice * removed unused variable (potentially broken if didn't set)

--- userbot/modules/afk.py
+++ userbot/modules/afk.py
@@ -48,7 +48,6 @@ AFKSTR = [
 ]
 
 # ================= CONSTANT =================
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 AFKSK = str(choice(AFKSTR))
 # ============================================
 
@@ -109,7 +108,8 @@ async def type_afk_is_not_true(notafk):
     user = await bot.get_me()
     last = user.last_name
     if last and last.endswith(" [OFF]"):
-        last1 = last[:-6]
+        raw_last = f"{last}"
+        last1 = raw_last.replace(" [OFF]", "")
     else:
         last1 = ""
     back_alive = datetime.now()
@@ -184,19 +184,19 @@ async def mention_afk(mention):
                 afk_since = f"`{int(seconds)}s`"
             if mention.sender_id not in USERS:
                 if AFKREASON:
-                    await mention.reply(f"**I've been AFK Since** {afk_since}.\
-                            \n**Reason:** `{AFKREASON}`")
+                    await mention.reply(f"**I've been AFK.** (Since {afk_since} ago.\
+                        \n**Reason:** `{AFKREASON}`")
                 else:
-                    await mention.reply(f"**I've been AFK Since** {afk_since}.\nReason: \n{AFKSK}\n`.` ")
+                    await mention.reply(f"{AFKSK}")
                 USERS.update({mention.sender_id: 1})
                 COUNT_MSG = COUNT_MSG + 1
             elif mention.sender_id in USERS:
                 if USERS[mention.sender_id] % randint(2, 4) == 0:
                     if AFKREASON:
-                        await mention.reply(f"**I've been AFK Since** {afk_since}.\
+                        await mention.reply(f"**I'm still AFK.** (Since {afk_since} ago.\
                             \n**Reason:** `{AFKREASON}`")
                     else:
-                        await mention.reply(f"**I've been AFK Since** {afk_since}.\nReason: \n{AFKSK}\n`.` ")
+                        await mention.reply(f"{AFKSK}")
                     USERS[mention.sender_id] = USERS[mention.sender_id] + 1
                     COUNT_MSG = COUNT_MSG + 1
                 else:
@@ -261,19 +261,19 @@ async def afk_on_pm(sender):
                 afk_since = f"`{int(seconds)}s`"
             if sender.sender_id not in USERS:
                 if AFKREASON:
-                    await sender.reply(f"I'm **I've been AFK Since** {afk_since}.\
-                        \n**Reason:** `{AFKREASON}`")
+                    await mention.reply(f"**I've been AFK.** (Since {afk_since} ago.\
+                       \n**Reason:** `{AFKREASON}`")
                 else:
-                    await sender.reply(f"I'm **I've been AFK Since** {afk_since}.\nReason: \n{AFKSK}\n`.` ")
+                    await sender.reply(f"{AFKSK}")
                 USERS.update({sender.sender_id: 1})
                 COUNT_MSG = COUNT_MSG + 1
             elif apprv and sender.sender_id in USERS:
                 if USERS[sender.sender_id] % randint(2, 4) == 0:
                     if AFKREASON:
-                        await sender.reply(f"I'm **still I've been AFK Since** {afk_since}.\
+                        await mention.reply(f"**I'm still AFK.** (Since {afk_since} ago.\
                             \n**Reason:** `{AFKREASON}`")
                     else:
-                        await sender.reply(f"I'm **I've been AFK Since** {afk_since}.\nReason: \n{AFKSK}\n`.` ")
+                        await sender.reply(f"{AFKSK}")
                     USERS[sender.sender_id] = USERS[sender.sender_id] + 1
                     COUNT_MSG = COUNT_MSG + 1
                 else:
