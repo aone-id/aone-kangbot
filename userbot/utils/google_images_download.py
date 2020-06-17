632b4faeb2a90448059edf5b0b4a66944795a39b
MoveAngel <moveangel@minaproject.tech>
2020-06-17 22:52:40 +0700

    utils: googleimagesdownload: Fixed problem \ at the end of the 'object_raw' string
    This solution from hardikvasa/google-images-download#318
    
    Signed-off-by: MoveAngel <moveangel@minaproject.tech>

--- userbot/utils/google_images_download.py
+++ userbot/utils/google_images_download.py
@@ -749,9 +749,8 @@ class googleimagesdownload:
         start_object = s.find('[', start_line + 1)
         end_object = s.find('</script>', start_object + 1) - 4
         object_raw = str(s[start_object:end_object])
-        object_decode = bytes(object_raw, "utf-8").decode("unicode_escape")
-        image_objects = json.loads(object_decode)[31][0][12][2]
-        return image_objects
+        object_decode = bytes(object_raw[:-1], "utf-8").decode("unicode_escape")
+        return json.loads(object_decode)[31][0][12][2]
 
     def _get_all_items(self,page,main_directory,dir_name,limit,arguments):
         items = []
