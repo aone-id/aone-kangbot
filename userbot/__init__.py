bee5d3d291e53e5d748a7afd3d693cde1df8a969
Azrim <mirzaspc@gmail.com>
2020-06-28 15:45:14 +0700

    Bump version to 2.7

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -52,7 +52,7 @@ if CONFIG_CHECK:
     quit(1)
 
 # Version
-BOT_VERSION = "2.6"
+BOT_VERSION = "2.7"
 AONE_LOGO = os.environ.get(
     "AONE_LOGO",
     "https://github.com/aone-id/aone-kangbot/raw/sql-extended/aoneid.png")
